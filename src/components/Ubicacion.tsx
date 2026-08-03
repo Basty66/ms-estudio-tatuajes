229c24e40388feba703297131fcb6bad444273db
Basty66 <cri.cerdaa@duocuc.cl>
2026-08-03 00:57:26 -0400

    fix: Google Maps embed showing correct address for Av. Conde de Manso 529, Melipilla

--- src/components/Ubicacion.tsx
+++ src/components/Ubicacion.tsx
@@ -77,7 +77,7 @@ export default function Ubicacion() {
           >
             <div className="w-full h-full min-h-[200px] rounded-2xl overflow-hidden">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!2d3325.5!2d-71.214!3d-33.685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDQxJzA2LjAiUyA3McKwMTInNTAuNCJX!5e0!3m2!1ses!2scl!4v1!4m1!1s0x9662b5b5b5b5b5b5%3A0x0!2zQXYuIE1hbnNvIDUyOSwgTWVsaXBpbGxh!5e0!3m2!1ses!2scl"
+                src="https://maps.google.com/maps?q=Av.+Conde+de+Manso+529,+Melipilla,+Chile&t=&z=17&ie=UTF8&iwloc=&output=embed"
                 width="100%"
                 height="100%"
                 allowFullScreen
